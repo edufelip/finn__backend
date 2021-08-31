30a08bdc4330942a8e6ff0c3945b30627c12d37f
Eduardo Felipe <edu_felip@hotmail.com>
2021-08-31 20:35:33 -0300

    add .env and changes to user model

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,13 +1,9 @@
 export class UserModel {
-    public readonly _id?: string
-
+    // public readonly _id?: string
+    public _id?: string
     public name: string
 
-    public email: string
-
-    public password: string
-
-    constructor(props: Omit<UserModel, '_id'>) {
-      Object.assign(this, props)
+    constructor() {
+      Object.assign(this)
     }
 }
