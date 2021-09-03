b92bd1c9b4d720a2f6b5ee6c3d570677e2b64dc9
Eduardo Felipe <edu_felip@hotmail.com>
2021-09-02 22:08:55 -0300

    add middleware to handle error

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,33 @@
 
 import express from 'express'
 import userRoutes from './routes/userRoutes'
+import postRoutes from './routes/postRoutes'
 
 class MainApp {
   public express
 
   constructor() {
     this.express = express()
-    this.middlewares()
+    this.express.use(express.json())
     this.setRoutes()
+    this.middlewares()
   }
 
   middlewares() {
-    this.express.use(express.json())
+    this.express.use(function(error, req, res, next) {
+      if (error.message === 'User already exists') {
+        res.status(409).send(error.message)
+      }
+      if (error.message === 'User not found') {
+        res.status(404).send(error.message)
+      }
+      res.status(500).send(error.message)
+    })
   }
 
   setRoutes() {
     this.express.use('/users', userRoutes)
+    this.express.use('/posts', postRoutes)
   }
 }
 
