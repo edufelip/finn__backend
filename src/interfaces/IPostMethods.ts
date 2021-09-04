8e6d32e97eb558dce9b465205d9eb45a6779cdcf
Eduardo Felipe <edu_felip@hotmail.com>
2021-09-04 20:35:58 -0300

    add post crud and methods

--- src/interfaces/IPostMethods.ts
+++ src/interfaces/IPostMethods.ts
@@ -1,7 +1,7 @@
 import { PostModel } from '@models/PostModel'
 
 export interface IPostMethods {
-    getPostsFeed: (id: string) => Promise<PostModel[]>
+    getPostsFeed: (id: string, page: string) => Promise<PostModel[]>
 
     getPostsFromUser: (id: string) => Promise<PostModel[]>
 
@@ -14,4 +14,10 @@ export interface IPostMethods {
     updatePost: (id: string, post: PostModel) => Promise<PostModel>
 
     deletePost: (id: string) => Promise<PostModel>
+
+    increaseLikePost: (user_id: string, post_id: string) => Promise<PostModel>
+
+    decreaseLikePost: (user_id: string, post_id: string) => Promise<PostModel>
+
+    getLikeCount: (post_id: string) => Promise<number>
 }
--- src/models/PostModel.ts
+++ src/models/PostModel.ts
@@ -2,9 +2,8 @@ export class PostModel {
     public readonly id?: string
     public content: string
     public photo: string
-    public communityId: string
-    public userId: string
-    public likesCount: number
+    public community_id: string
+    public user_id: string
 
     constructor(props: Omit<PostModel, 'id'>) {
       Object.assign(this, props)
